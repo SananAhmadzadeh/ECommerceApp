835922810058e1068e098d54a2a830251658e03e
sanan13ahmadzada <geniusenan@gmail.com>
2026-04-26 17:55:47 +0400

    init

---
+++ ECommerceAPI/Presentation/ECommerceAPI.API/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("get")]
+        public IActionResult Get()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+    }
+}
